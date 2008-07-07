ceacf573357157e7dac48783e7eee5f6cc534a4e
Török Gábor <gabor@20y.hu>
2008-07-07 18:11:25 +0200

    emacs customization file added

---
+++ .emacs-custom.el
@@ -0,0 +1,40 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(auto-compression-mode t nil (jka-compr))
+ '(auto-fill-mode t)
+ '(blink-cursor-mode nil)
+ '(browse-url-browser-function (quote w3m-browse-url))
+ '(case-fold-search t)
+ '(column-number-mode t)
+ '(current-language-environment "UTF-8")
+ '(default-input-method "rfc1345")
+ '(display-time-default-load-average nil)
+ '(display-time-format "%y.%m.%d. %H.%M")
+ '(display-time-mode nil)
+ '(ecb-options-version "2.32")
+ '(fill-column 79)
+ '(global-font-lock-mode t nil (font-lock))
+ '(global-hl-line-mode t)
+ '(kill-whole-line t)
+ '(menu-bar-mode t)
+ '(mouse-yank-at-point t)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t nil (paren))
+ '(show-paren-style (quote mixed))
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil)
+ '(transient-mark-mode t)
+ '(weblogger-config-alist (quote (("20y" ("user" . "admin") ("server-url" . "http://20y.hu/xmlrpc.php") ("weblog" . "1")))))
+ '(webma-instance-server "neotek.hu"))
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(bold ((t (:bold t :background "LemonChiffon"))))
+ '(show-paren-match ((t (:bold t :background "#eeca00" :foreground "#763b17"))))
+ '(w3m-bold ((t (:bold t :background "LemonChiffon" :foreground "#453e06")))))
