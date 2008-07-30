15f0719001535e1725b684a0a9cac7fca128531f
Török Gábor <gabor@20y.hu>
2008-07-30 16:31:50 +0200

    Add endofday.sh to repo.

---
+++ bin/endofday.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+GIT_REPOS=("/home/gabor/dev/webma/" "/home/gabor/" "/home/gabor/slink/")
+
+for ((i = 1; i <= $#GIT_REPOS; i++ )) {
+    cd $GIT_REPOS[$i]
+    nr=`git diff --raw | wc -l`
+    if [ $nr -eq 0 ]; then
+        echo Pushing commits from $GIT_REPOS[$i].
+        git push
+    else
+        echo $nr uncommited changes in $GIT_REPOS[$i].
+    fi
+    cd -
+}
