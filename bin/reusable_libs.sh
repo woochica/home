527c9919b8b768930c7b93da62c0299339ebc7a1
Török Gábor <gabor@20y.hu>
2008-11-24 10:13:26 +0100

    Fix zsh bug.

--- bin/reusable_libs.sh
+++ bin/reusable_libs.sh
@@ -18,16 +18,25 @@ include()
     pattern="${1##*.}"
     # If only library name was given, load all the scripts
     if [ "$pattern" = "$library" ] ; then
-        pattern='*'
+        pattern="*"
     fi
     library_dir=~/bin/$library.lib
 
     # Process scripts in library according to given pattern
     if [ -d $library_dir ] ; then
-        for script in $library_dir/$pattern.sh ; do
-            echo "Loading script \"$script\"..."
-            . $script > /dev/null
-        done
+        # Zsh way
+        if [ $pattern = "*" ] ; then
+            for script in $library_dir/$~pattern.sh; do
+                echo "Loading script \"$script\"..."
+                . $script > /dev/null
+            done
+        # Bash way
+        else
+            for script in $library_dir/$pattern.sh; do
+                echo "Loading script \"$script\"..."
+                . $script > /dev/null
+            done
+        fi
     else
         echo $library_dir": No such library found"
     fi
