b5fd8960608eb88117520aca39a7eb755733e351
Török Gábor <gabor@20y.hu>
2008-12-19 13:23:17 +0100

    List of packages to install on a fresh Fedora installation.

---
+++ bin/yum_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Packages to install on a fresh Fedora installation
+
+PACKAGES=(yum install linux-libertine-fonts.noarch baekmuk-ttf-fonts-common.noarch ogg123 mplayer w3m rcs ncftp gcc-c++ tidy wine cabextract mysql httpd mysql-server nmap ipython python-gdata yum-fastestmirror yum-presto git cvs fuse-sshfs.i386 libdvdread libdvdnav lsdvd gstreamer-plugins-good gstreamer-plugins-bad gstreamer-plugins-ugly gcc flash-plugin  screen libXpm-devel.i386 libpng10-devel.i386 libtiff-devel.i386 jpgalleg-devel.i386 libjpeg-devel.i386 libungif-devel libpng-devel php-cli php liberation-fonts flash-plugin most vala vala-tools svn dbus-glib-devel.i386 mod_suphp curl)
