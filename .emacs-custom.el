fe544073949d8c8d3c665b9be015f95182a8663a
Török Gábor <gabor@20y.hu>
2009-02-24 18:31:45 +0100

    Update faces.

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -48,9 +48,11 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(css-selector ((t (:inherit font-lock-function-name-face :weight bold))))
  '(flymake-errline ((t (:background "black" :underline "red4" :weight bold))))
  '(font-lock-comment-face ((t (:foreground "IndianRed" :slant normal :weight normal :height 1.0 :family "Andale Mono"))))
  '(show-paren-match ((t (:bold t :background "#eeca00" :foreground "#763b17"))))
  '(twit-title-face ((t (:underline "DeepSkyBlue"))))
  '(twit-zebra-1-face ((t nil)))
- '(twit-zebra-2-face ((t nil))))
+ '(twit-zebra-2-face ((t nil)))
+ '(which-func ((((class color) (min-colors 88) (background dark)) (:foreground "Pink")))))
